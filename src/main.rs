1555b659c56162f8ea40de94f919fa4ad4ac13fe
akane10 <targaryen.akane@gmail.com>
2021-10-22 00:30:51 +0700

    use unwrap_or

--- src/main.rs
+++ src/main.rs
@@ -20,12 +20,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync + 'static>
     dotenv::dotenv().ok();
 
     let filename = Path::new(env!("CARGO_MANIFEST_DIR")).join("last_time.txt");
-    let time = fs::read_to_string(&filename);
-
-    let mut last_time = match time {
-        Ok(t) => t,
-        _ => String::new(),
-    };
+    let mut last_time = fs::read_to_string(&filename).unwrap_or(String::new());
 
     let mut interval = time::interval(time::Duration::from_secs(60));
     loop {
